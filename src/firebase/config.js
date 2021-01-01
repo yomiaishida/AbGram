c223b5eed68a1d9d692d86fa3c5194a674c6c166
Abayomi Aishida <yomicorperate@gmail.com>
2021-01-01 19:17:29 +0100

    Uploading files in firebase storage and generating links with firebase cloud firestore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,4 @@
-import * as firebase from "firebase/app";
+import firebase from "firebase/app";
 import "firebase/storage";
 import "firebase/firestore";
 
@@ -15,9 +15,10 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+// firebase.analytics();
 
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { projectStorage, projectFirestore };
+export { projectStorage, projectFirestore, timestamp };
--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,14 +1,19 @@
 import { useState, useEffect } from "react";
-import { projectStorage } from "../firebase/config";
+import {
+  projectStorage,
+  projectFirestore,
+  timestamp,
+} from "../firebase/config";
 
 const useStorage = (file) => {
   const [progress, setProgress] = useState(0);
   const [error, setError] = useState(null);
-  const [ulr, setUrl] = useState(null);
+  const [url, setUrl] = useState(null);
 
   useEffect(() => {
     // rederences
     const storageRef = projectStorage.ref(file.name);
+    const collectionRef = projectFirestore.collection("images");
 
     storageRef.put(file).on(
       "state_changed",
@@ -20,7 +25,9 @@ const useStorage = (file) => {
         setError(err);
       },
       async () => {
-        const url = await storageRef.getDownloadUrl();
+        const url = await storageRef.getDownloadURL();
+        const createdAt = timestamp();
+        collectionRef.add({ url, createdAt });
         setUrl(url);
       }
     );
